870cd2709245ec1dbd9d2107f01c4a52313d7f97
Sebastian Forero <sebastian.forero.77@gmail.com>
2022-09-21 21:14:40 -0500

    Script con carga de poligonos y puntos

--- LAB1/conexion.py
+++ LAB1/conexion.py
@@ -8,15 +8,22 @@ print(conn)
 cur = conn.cursor()
 print(cur)
 
-with open('export.geojson') as f:
+with open('export2.geojson') as f:
     gj = geojson.load(f)
 
 #for feature in gj['features']:
-#    print(feature["properties"]["name"])
-    
+#    print(feature['geometry'])
+#    print(json.dumps(feature['geometry']['type']=='Point'))
 
 for feature in gj['features']:
-    cur.execute("INSERT INTO museum (name, geom) VALUES (%s, ST_GeomFromGeoJSON(%s))", (feature['properties']['name'], json.dumps(feature['geometry'])))
+    if feature['geometry']['type']=='Point':
+        cur.execute("INSERT INTO museum (name, geom_point) VALUES (%s, ST_GeomFromGeoJSON(%s))", (feature['properties']['name'], json.dumps(feature['geometry'])))
+    elif feature['geometry']['type']=='Polygon':
+        cur.execute("INSERT INTO museum (name, geom) VALUES (%s, ST_GeomFromGeoJSON(%s))", (feature['properties']['name'], json.dumps(feature['geometry'])))
+
+
+#for feature in gj['features']:
+#    cur.execute("INSERT INTO museum (name, geom) VALUES (%s, ST_GeomFromGeoJSON(%s))", (feature['properties']['name'], json.dumps(feature['geometry'])))
 conn.commit()       
 
 cur.close()
