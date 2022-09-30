ce27e310f9bceb768baae2497e1ff681388d6463
Sebastian Forero <sebastian.forero.77@gmail.com>
2022-09-30 15:41:13 -0500

    Trabajo Clase

--- LAB1/conexion_vias.py
+++ LAB1/conexion_vias.py
@@ -17,22 +17,14 @@ with open('/home/sebas/Documentos/Bases Datos Espaciales/LAB1/calles.geojson') a
 
 for feature in gj['features']:
     if feature['geometry']['type']=='LineString':
-        #print("({}, ST_GeomFromGeoJSON{},{})".format(feature['properties']['name'], json.dumps(feature['geometry']), feature['properties']['highway']))
-        #print("INSERT INTO autopista (name, geometria, feature) VALUES ('{}', ST_GeomFromGeoJSON('{}'),'{}')".format(feature['properties']['name'], json.dumps(feature['geometry']), feature['properties']['highway']))
+        #print( feature['properties']['name'])
         cur.execute("INSERT INTO autopista (name, geometria, feature) VALUES ('{}', ST_GeomFromGeoJSON('{}'),'{}')".format(feature['properties']['name'], json.dumps(feature['geometry']), feature['properties']['highway']))
-        #print( feature['properties']['name'])
     elif feature['geometry']['type']=='Polygon':
-        #print("({}, ST_GeomFromGeoJSON{},{})".format(feature['properties']['name'], json.dumps(feature['geometry']), feature['properties']['highway']))
         #print(feature['properties']['name'])
-        #print("INSERT INTO autopista (name, geometria, feature) VALUES ('{}', ST_GeomFromGeoJSON('{}'),'{}')".format(feature['properties']['name'], json.dumps(feature['geometry']), feature['properties']['highway']))
         cur.execute("INSERT INTO autopista (name, geometria_pol, feature) VALUES ('{}', ST_GeomFromGeoJSON('{}'),'{}')".format(feature['properties']['name'], json.dumps(feature['geometry']), feature['properties']['highway']))
-        #print( feature['properties']['name'])
     else:
         print("hay otro tipo de dato")
 
-
-#for feature in gj['features']:
-#    cur.execute("INSERT INTO museum (name, geom) VALUES (%s, ST_GeomFromGeoJSON(%s))", (feature['properties']['name'], json.dumps(feature['geometry'])))
 conn.commit()       
 cur.close()
 conn.close()
